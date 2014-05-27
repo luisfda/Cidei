9e8d0ac2cbb0f28b78abc302bf59a58cc3ab922b
Luis Daza <ldaza@cidei.net>
2014-05-27 18:11:57 -0500

    fda

--- Frontend/lab5/callback.js
+++ Frontend/lab5/callback.js
@@ -3,10 +3,10 @@
 *y la función que lo recibe espera que se ejecute ésta función
 */
 function algo(miCallback){
-	micallback();
+	miCallback();
 }
 
-algo(function())
+algo(function()
 {
 	console.log("Esto es un callback");	
 });
@@ -19,3 +19,29 @@ otroCallback(function(unValor)
 {
 	console.log("algo se imprime y es: "+unValor );
 });
+
+/*multiples callbacks llamado en determinados casos*/
+
+function algoOcurre(callback1, callback2, callback3){
+	//Pasa la primera vez
+	callback1("Primer paso");
+
+	//Siguiente callback
+	callback2("Segundo paso");
+
+	//Y por último
+	callback3("Ultimo y tercer paso");
+}
+
+//LLamado y uso de múltiples callback para "algoOcurre"
+algoOcurre(
+	function(paso1){
+		console.log(paso1);
+	},
+	function(paso2){
+		console.log(paso2);
+	},
+	function(paso3){
+		console.log(paso3);
+	}
+);
